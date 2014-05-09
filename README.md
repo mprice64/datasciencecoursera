e4548758737166ebbfea498303ea570426796655
U-robo\user <user@robo.(none)>
2014-05-08 17:04:17 -0700

    Brand new repo with a little scratch demo test code based on the intro lectures

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-datasciencecoursera
+The Data Scientist's Toolbox coursework
 ===================
+
+This is for the class at https://class.coursera.org/datascitoolbox-003
---
+++ scratch/intro.R
@@ -0,0 +1,50 @@
+# Install devtools from http://cran.r-project.org/bin/windows/Rtools/
+
+version
+
+install.packages("caret")
+install.packages("kernlab")
+install.packages("devtools")
+
+library(devtools)
+install_github('rCharts', 'ramnathv')
+
+library(datasets)
+library(rCharts)
+
+# Rcharts examples
+## Example 1 Facetted Scatterplot
+names(iris) = gsub("\\.", "", names(iris))
+rPlot(SepalLength ~ SepalWidth | Species, data = iris, color = 'Species', type = 'point')
+
+## Example 2 Facetted Barplot
+hair_eye = as.data.frame(HairEyeColor)
+rPlot(Freq ~ Hair | Eye, color = 'Eye', data = hair_eye, type = 'bar')
+
+# Reading from the web
+con <- url("http://www.jhsph.edu","r")
+x <- readLines( con )
+head(x)
+
+# K-means clustering
+set.seed(1234)
+par(mar = c(0, 0, 0, 0))
+x <- rnorm(12, mean = rep(1:3, each = 4), sd = 0.2)
+y <- rnorm(12, mean = rep(c(1, 2, 1), each = 4), sd = 0.2)
+plot(x, y, col = "blue", pch = 19, cex = 2)
+text(x + 0.05, y + 0.05, labels = as.character(1:12))
+
+??regression
+
+# Correlated predictors
+
+library(caret)
+library(kernlab)
+data(spam)
+inTrain <- createDataPartition(y = spam$type, p = 0.75, list = FALSE)
+training <- spam[inTrain, ]
+testing <- spam[-inTrain, ]
+
+M <- abs(cor(training[, -58]))
+diag(M) <- 0
+which(M > 0.8, arr.ind = T)
