e6fa0e9dbaea284c7669a071278ec9a9edcf4c51
Martin Price <64mprice@gmail.com>
2014-05-10 13:43:06 -0700

    More libraries

--- scratch/InstallLibraries.R
+++ scratch/InstallLibraries.R
@@ -4,19 +4,16 @@ version
 
 install.packages("caret")
 install.packages("kernlab")
-install.packages("devtools")
 install.packages("knitr")
 install.packages("KernSmooth")
+install.packages("devtools")
+install.packages("swirl")
+install.packages("hexbin")
+install.packages("ggplot2")
 
 library(devtools)
 find_rtools()
-install_github('rCharts', 'ramnathv')
 
+install_github('rCharts', 'ramnathv')
 install_github("slidify", "ramnathv")
 install_github("slidifyLibraries", "ramnathv")
-
-library(datasets)
-library(rCharts)
-library(KernSmooth)
-
-search()
